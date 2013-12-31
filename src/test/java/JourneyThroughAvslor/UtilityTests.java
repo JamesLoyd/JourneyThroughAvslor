a76ba4945dcbf9d6bc00df445d0588656afbd7eb
Joshua Theze <fox@foxiepa.ws>
2013-12-31 12:41:43 -0500

    Ok, Now we should have passing builds and tests.

--- src/test/java/JourneyThroughAvslor/UtilityTests.java
+++ src/test/java/JourneyThroughAvslor/UtilityTests.java
@@ -1,6 +1,5 @@
 package JourneyThroughAvslor;
 
-import static org.junit.Assert.AssertEquals;
 import org.junit.Test;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
@@ -15,6 +14,6 @@ public class UtilityTests {
 
     @Test
     public void testAttackDamage() {
-        AssertEquals("Failure, Math is wrong", JourneyThroughAvslor.Utility.getAttackDamage(2.0,2.0), 4.0);
+        org.junit.Assert.assertEquals(4.0, JourneyThroughAvslor.Utility.getAttackDamage(2.0,2.0),0.0);
     }
 }
