2eca5d5061d5882d4cdb7ac04d81ac4980703481
James Loyd <jamestloyd@gmail.com>
2014-02-04 16:58:55 -0500

    Now to fix that build

--- src/main/java/JourneyThroughAvslor/Engine/Map/MapEngine.java
+++ src/main/java/JourneyThroughAvslor/Engine/Map/MapEngine.java
@@ -13,7 +13,7 @@ package JourneyThroughAvslor.Engine.Map;
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
- public Class MapEngine implements Runnable
+ public class MapEngine implements Runnable
  {
     public void run()
     {
