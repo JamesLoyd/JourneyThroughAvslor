98f092506caa9b585bcb6fd02a5851bdff92f253
James <jamestloyd@gmail.com>
2014-02-06 17:57:33 -0500

    removing not needed code

--- src/main/java/JourneyThroughAvslor/Engine/Shared/MapReader.java
+++ src/main/java/JourneyThroughAvslor/Engine/Shared/MapReader.java
@@ -16,16 +16,11 @@ import org.json.JSONObject;
  */
 public class MapReader
 {
-    private MapReader(String file)
+    private MapReader()
     {
 
     }
-    public void returnStatus()
-    {
-        System.out.println("Not implemented yet");
-    }
-
-    public static MapReader createMapReader(String file) {
-        return new MapReader(file);
+    public static MapReader createMapReader() {
+        return new MapReader();
     }
 }
