625bd279a776f1bb27971be659043a0b3a8b6211
James <jamestloyd@gmail.com>
2014-02-05 20:43:40 -0500

    Updated the JSON package support

--- src/main/java/org/json/JSONStringer.java
+++ src/main/java/org/json/JSONStringer.java
@@ -32,7 +32,7 @@ import java.io.StringWriter;
  * The texts produced strictly conform to org syntax rules. No whitespace is
  * added, so the results are ready for transmission or storage. Each instance of
  * JSONStringer can produce one org text.
- * <p>
+ * <p/>
  * A JSONStringer instance provides a <code>value</code> method for appending
  * values to the
  * text, and a <code>key</code>
@@ -48,12 +48,13 @@ import java.io.StringWriter;
  *     .endObject()
  *     .toString();</pre> which produces the string <pre>
  * {"org":"Hello, World!"}</pre>
- * <p>
+ * <p/>
  * The first method called must be <code>array</code> or <code>object</code>.
  * There are no methods for adding commas or colons. JSONStringer adds them for
  * you. Objects and arrays can be nested up to 20 levels deep.
- * <p>
+ * <p/>
  * This can sometimes be easier than using a JSONObject to build a string.
+ *
  * @author org.org
  * @version 2008-09-18
  */
@@ -71,6 +72,7 @@ public class JSONStringer extends JSONWriter {
      * problem in the construction of the org text (such as the calls to
      * <code>array</code> were not properly balanced with calls to
      * <code>endArray</code>).
+     *
      * @return The org text.
      */
     public String toString() {
