b64036cf08431fc57e8a4d40cc5be45d848c0a8b
James <jamestloyd@gmail.com>
2014-02-11 20:58:30 -0500

    small refractor plus new interface!

--- src/main/java/org/avslor/JourneyThroughAvslor/Engine/Shared/Events/EventHandler.java
+++ src/main/java/org/avslor/JourneyThroughAvslor/Engine/Shared/Events/EventHandler.java
@@ -1,4 +1,4 @@
-package org.avslor.JourneyThroughAvslor.Engine.Shared;
+package org.avslor.JourneyThroughAvslor.Engine.Shared.Events;
 /*   Copyright 2013 James Loyd , Joshua Theze
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
