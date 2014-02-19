c46fc6790e7491aff65acd6366b332ebdd02d52b
James <jamestloyd@gmail.com>
2014-02-19 17:19:58 -0500

    refactor gameengine.java

--- src/main/java/org/avslor/JourneyThroughAvslor/Engine/Shared/GameEngine.java
+++ src/main/java/org/avslor/JourneyThroughAvslor/Engine/Shared/GameEngine.java
@@ -1,4 +1,6 @@
 package org.avslor.JourneyThroughAvslor.Engine.Shared;
+import org.avslor.JourneyThroughAvslor.Engine.Shared.Events.Event;
+import org.avslor.JourneyThroughAvslor.Engine.Shared.Events.EventHandler;
 /*   Copyright 2013 James Loyd , Joshua Theze
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,10 +15,6 @@ package org.avslor.JourneyThroughAvslor.Engine.Shared;
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
-
-import org.avslor.JourneyThroughAvslor.Engine.Shared.Events.Event;
-import org.avslor.JourneyThroughAvslor.Engine.Shared.Events.EventHandler;
-
 public class GameEngine implements Runnable
 {
     public void run()
